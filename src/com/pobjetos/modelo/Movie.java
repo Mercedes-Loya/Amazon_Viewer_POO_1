9a8feaa6fc63bddc1163a2653da9d5bf67f8d774
Mercedes-Loya <maria_loya27@yahoo.com>
2018-10-31 09:07:52 -0500

    Agregado atributos de Movie

--- src/com/pobjetos/modelo/Movie.java
+++ src/com/pobjetos/modelo/Movie.java
@@ -6,4 +6,142 @@ package com.pobjetos.modelo;
  */
 public class Movie {
 
+    private int id;
+    private String title;
+    private String genre;
+    private String creador;
+    private int duration;
+    private short year;
+    private boolean viewed;
+    private int timeViewed;
+
+    public Movie() {
+    }
+
+    public Movie( String title, String genre, String creador, int duration, short year) {
+       
+        this.title = title;
+        this.genre = genre;
+        this.creador = creador;
+        this.duration = duration;
+        this.year = year;
+       
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return the creador
+     */
+    public String getCreador() {
+        return creador;
+    }
+
+    /**
+     * @param creador the creador to set
+     */
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+    /**
+     * @return the duration
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * @param duration the duration to set
+     */
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * @return the year
+     */
+    public short getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the viewed
+     */
+    public boolean getIsViewed() {
+        return viewed;
+    }
+
+    /**
+     * @param viewed the viewed to set
+     */
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    /**
+     * @return the timeViewed
+     */
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    /**
+     * @param timeViewed the timeViewed to set
+     */
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+    
+    
+    
 }
+
+
