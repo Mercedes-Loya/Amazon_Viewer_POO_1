e1530f46b2deaffc83c07bfba33c9fda5c02a194
Mercedes-Loya <maria_loya27@yahoo.com>
2018-10-31 09:12:00 -0500

    Agregado atributos de serie

--- src/com/pobjetos/modelo/Serie.java
+++ src/com/pobjetos/modelo/Serie.java
@@ -5,5 +5,155 @@ package com.pobjetos.modelo;
  * @author SSolis
  */
 public class Serie {
+     private int id;
+    private String title;
+    private String genre;
+    private String creador;
+    private int duration;
+    private short year;
+    private boolean viewed;
+    private int sessionQuantity;
+    private Chapter chapters;
 
+    public Serie() {
+    }
+
+    public Serie( String title, String genre, String creador, int duration, short year, int sessionQuantity, Chapter chapters) {
+        
+        this.title = title;
+        this.genre = genre;
+        this.creador = creador;
+        this.duration = duration;
+        this.year = year;
+        this.sessionQuantity = sessionQuantity;
+        this.chapters = chapters;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    /**
+     * @return the creador
+     */
+    public String getCreador() {
+        return creador;
+    }
+
+    /**
+     * @param creador the creador to set
+     */
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+    /**
+     * @return the duration
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * @param duration the duration to set
+     */
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * @return the year
+     */
+    public short getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    /**
+     * @return the viewed
+     */
+    public boolean getIsViewed() {
+        return viewed;
+    }
+
+    /**
+     * @param viewed the viewed to set
+     */
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    /**
+     * @return the sessionQuantity
+     */
+    public int getSessionQuantity() {
+        return sessionQuantity;
+    }
+
+    /**
+     * @param sessionQuantity the sessionQuantity to set
+     */
+    public void setSessionQuantity(int sessionQuantity) {
+        this.sessionQuantity = sessionQuantity;
+    }
+
+    /**
+     * @return the chapters
+     */
+    public Chapter getChapters() {
+        return chapters;
+    }
+
+    /**
+     * @param chapters the chapters to set
+     */
+    public void setChapters(Chapter chapters) {
+        this.chapters = chapters;
+    }
+    
 }
+
